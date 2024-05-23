0d114d3572c5d83a8e8fd4e4a5708bbb02ccfb2f
Billy Alexander <billyalexndr@gmail.com>
2024-05-23 23:37:17 +0700

    make test passed

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -4,10 +4,10 @@ function useInput(defaultValue = '') {
     const [value, setValue] = useState(defaultValue);
 
     function handleValueChange({ target }) {
-        setValue(target.value);
-    }
+    setValue(target.value);
+}
 
-    return [value, handleValueChange, setValue];
+return [value, handleValueChange, setValue];
 }
 
 export default useInput;
